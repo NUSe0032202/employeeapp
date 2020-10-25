a987177e05b2bba5bbc492c5987b537f44757caf
Ben <benjaminasj@hotmail.com>
2020-10-26 00:20:34 +0800

    Added unit tests for backend

--- src/main/java/com/cognizant/backend/employeeapp/modal/Employee.java
+++ src/main/java/com/cognizant/backend/employeeapp/modal/Employee.java
@@ -6,31 +6,48 @@ public class Employee {
 	private String name;
 	private String login;
 	private Double salary;
-	
+
+	public Employee() {
+
+	}
+
+	public Employee(String id, String name, String login, Double salary) {
+		this.id = id;
+		this.name = name;
+		this.login = login;
+		this.salary = salary;
+	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getLogin() {
 		return login;
 	}
+
 	public void setLogin(String login) {
 		this.login = login;
 	}
+
 	public Double getSalary() {
 		return salary;
 	}
+
 	public void setSalary(Double salary) {
 		this.salary = salary;
 	}
 
-	
 }
--- src/test/java/com/cognizant/backend/employeeapp/EmployeeappApplicationTests.java
+++ src/test/java/com/cognizant/backend/employeeapp/EmployeeappApplicationTests.java
@@ -1,13 +1,79 @@
 package com.cognizant.backend.employeeapp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
 
-@SpringBootTest
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.cognizant.backend.employeeapp.modal.Employee;
+import com.cognizant.backend.employeeapp.service.StoreRetrieveService;
+
 class EmployeeappApplicationTests {
 
+	DataSource dataSource;
+
+	StoreRetrieveService service;
+
+	JdbcTemplate jdbcTemplate;
+
+	@BeforeEach
+	public void setup() {
+		dataSource = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).generateUniqueName(true)
+				.addScript("classpath:schema.sql").build();
+		service = new StoreRetrieveService();
+		jdbcTemplate = new JdbcTemplate(dataSource);
+		ReflectionTestUtils.setField(service, "jdbcTemplate", jdbcTemplate);
+	}
+
 	@Test
-	void contextLoads() {
+	public void test_SuccessfulInsert() {
+		Employee emp1 = new Employee("e001", "peter", "guest123", 100.0);
+		Employee emp2 = new Employee("e002", "jane", "qwerty", 71263.0);
+
+		// Get test data into String array form
+		String[] input1 = { emp1.getId(), emp1.getLogin(), emp1.getName(), Double.toString(emp1.getSalary()) };
+		String[] input2 = { emp2.getId(), emp2.getLogin(), emp2.getName(), Double.toString(emp2.getSalary()) };
+
+		service.insert(input1);
+		service.insert(input2);
+
+		List<Employee> data = service.retrieveFilteredEmployees(0, 99999, 0, 2, "+ID");
+
+		assertThat(data).isNotEmpty();
+		assertThat(data)
+				.extracting(emp -> emp.getId(), emp -> emp.getName(), emp -> emp.getLogin(), emp -> emp.getSalary())
+				.containsExactly(tuple("e001", "peter", "guest123", 100.0), tuple("e002", "jane", "qwerty", 71263.0));
+	}
+
+	@Test
+	public void test_ExistingRecords() {
+		Employee emp1 = new Employee("e001", "peter", "guest123", 100.0);
+		Employee emp2 = new Employee("e001", "peter", "qwerty", 71263.0);
+		
+		String[] input1 = { emp1.getId(), emp1.getLogin(), emp1.getName(), Double.toString(emp1.getSalary()) };
+		String[] input2 = { emp2.getId(), emp2.getLogin(), emp2.getName(), Double.toString(emp2.getSalary()) };
+        
+		//Assert that an error is thrown due to same employee id
+		assertThatThrownBy(() -> {
+			 service.insert(input1);
+ 		     service.insert(input2);
+		}).isInstanceOf(DuplicateKeyException.class);
+		
+		List<Employee> data = service.retrieveFilteredEmployees(0, 99999, 0, 2, "+ID");
+        
+		assertThat(data).isNotEmpty();
+		assertThat(data)
+		.extracting(emp -> emp.getId(), emp -> emp.getName(), emp -> emp.getLogin(), emp -> emp.getSalary())
+		.containsExactly(tuple("e001", "peter", "qwerty", 71263.0));
 	}
 
 }
